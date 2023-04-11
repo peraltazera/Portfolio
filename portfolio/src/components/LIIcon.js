5d0e2f9da5e6440fab11459b42bb6370d950866b
Victor Pereira <victorgamedeveloper@gmail.com>
2023-04-11 12:32:55 -0300

    circle
    circle

--- portfolio/src/components/LIIcon.js
+++ portfolio/src/components/LIIcon.js
@@ -6,15 +6,15 @@ const LIIcon = ({reference}) => {
     const {scrollYProgress} = useScroll(
         {
             target: reference,
-            offset: ["center end", "start center"]
+            offset: ["center end", "center start"]
         }
     )
 
   return (
     <figure className=' absolute left-0 stroke-primary'>
         <svg className=' -rotate-90' width="75px" height="75px" viewBox='0 0 100 100'>
-            <circle cx="75" cy="50" r="20" className=' stroke-violet-400 stroke fill-none' />
-            <motion.circle style={{pathLength:scrollYProgress}} cx="75" cy="50" r="40" className=' stroke-[5px] fill-dark' />
+            {/* <circle cx="75" cy="50" r="20" className=' stroke-violet-400 stroke fill-none' /> */}
+            <motion.circle style={{pathLength:scrollYProgress}} cx="75" cy="50" r="20" className=' stroke-[5px] fill-dark' />
             <circle cx="75" cy="50" r="10" className=' stroke-1 fill-light' />
         </svg>
     </figure>

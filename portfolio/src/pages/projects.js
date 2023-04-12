5b5ea5a7127ca6bde12ab8f581be031eb79705f2
Victor Pereira <victorgamedeveloper@gmail.com>
2023-04-12 11:43:54 -0300

    apk
    apk

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -4,6 +4,8 @@ import Layout from './Layout'
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import {motion} from 'framer-motion'
+// import {motion} from '../../public/downloads/PixelChess.rar'
+
 
 const projects = () => {
   return (
@@ -25,7 +27,7 @@ const projects = () => {
                 <h2 className=' font-bold text-4xl w-full text-light mt-4'>Open chat<span className=' text-primary'> &nbsp;|&nbsp; </span>Web</h2>
                 <p className=' font-medium text-base md:text-lg text-light'>O Open Chat é uma plataforma aberta que permite que você se conecte com qualquer pessoa 
                 cadastrada nela através do Gmail. Com essa ferramenta, é possível enviar mensagens para amigos, colegas de trabalho e outras pessoas da sua rede.</p>
-                <p className=' font-medium text-base md:text-lg text-light/60'>Web &nbsp;|&nbsp; Javascript &nbsp;|&nbsp; HTML &nbsp;|&nbsp; Tailwind &nbsp;|&nbsp; ReactJs &nbsp;|&nbsp; NextJs</p>
+                <p className=' font-medium text-base md:text-lg text-light/60'>Web &nbsp;|&nbsp; Javascript &nbsp;|&nbsp; HTML &nbsp;|&nbsp; CSS &nbsp;|&nbsp; React &nbsp;|&nbsp; Next.js</p>
                 <div className=' flex items-center self-start gap-6'>
                   <Link href='https://open-chat-victorpereira.vercel.app/' target={'_blank'} className=' flex items-center text-primary p-2 px-6 rounded-lg text-lg bg-dark border-primary
                   font-semibold hover:bg-primary hover:drop-shadow-primary hover:text-dark border-2 border-solid hover:border-primary'>Acessar</Link>
@@ -42,7 +44,7 @@ const projects = () => {
                 <p className=' font-medium text-base md:text-lg text-light'>O Chess Bot é um bot para Discord que possibilita a criação de partidas de xadrez em seu canal, 
                 permitindo que você jogue com seus amigos ou até mesmo contra a inteligência artificial. Com o Chess Bot, é possível desfrutar de uma experiência divertida e 
                 desafiadora de xadrez diretamente no Discord. Experimente o Chess Bot e eleve seu jogo de xadrez para o próximo nível!</p>
-                <p className=' font-medium text-base md:text-lg text-light/60'>Discord &nbsp;|&nbsp; Javascript &nbsp;|&nbsp; Discord.js &nbsp;|&nbsp; Bot &nbsp;|&nbsp; NodeJs &nbsp;|&nbsp; CanvasJs</p>
+                <p className=' font-medium text-base md:text-lg text-light/60'>Discord &nbsp;|&nbsp; Javascript &nbsp;|&nbsp; Discord.js &nbsp;|&nbsp; Bot &nbsp;|&nbsp; Node.js &nbsp;|&nbsp; CanvasJs</p>
                 <div className=' flex items-center self-start gap-6'>
                   <Link href='https://chess-bot-discord-victorpereira.vercel.app/' target={'_blank'} className=' flex items-center text-primary p-2 px-6 rounded-lg text-lg bg-dark border-primary
                   font-semibold hover:bg-primary hover:drop-shadow-primary hover:text-dark border-2 border-solid hover:border-primary'>Acessar</Link>
@@ -67,8 +69,10 @@ const projects = () => {
                   font-semibold hover:bg-primary hover:drop-shadow-primary hover:text-dark border-2 border-solid hover:border-primary'>Acessar</Link>
                    <Link href='https://github.com/peraltazera/Chess' target={'_blank'} className=' flex items-center text-primary p-2 px-6 rounded-lg text-lg bg-dark border-primary
                   font-semibold hover:bg-primary hover:drop-shadow-primary hover:text-dark border-2 border-solid hover:border-primary'>GitHub</Link>
-                   <Link href='https://github.com/peraltazera/Chess' className=' flex items-center text-primary p-2 px-6 rounded-lg text-lg bg-dark border-primary
-                  font-semibold hover:bg-primary hover:drop-shadow-primary hover:text-dark border-2 border-solid hover:border-primary'>Download</Link>
+                   <Link href='/downloads/PixelChess.rar' className=' flex items-center text-primary p-2 px-6 rounded-lg text-lg bg-dark border-primary
+                  font-semibold hover:bg-primary hover:drop-shadow-primary hover:text-dark border-2 border-solid hover:border-primary' download={true}>Windowns</Link>
+                   <Link href='/downloads/PixelChess.apk' className=' flex items-center text-primary p-2 px-6 rounded-lg text-lg bg-dark border-primary
+                  font-semibold hover:bg-primary hover:drop-shadow-primary hover:text-dark border-2 border-solid hover:border-primary' download={true}>Android</Link>
                 </div>
               </div>
             </motion.div>
